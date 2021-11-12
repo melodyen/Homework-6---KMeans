d102310124ea2fabffbe302d245460d35fd6c2dc
melodyen <91228601+melodyen@users.noreply.github.com>
2021-11-11 22:31:16 -0800

    Add files via upload

---
+++ Homework6.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import altair as alt
+import streamlit as st
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+
+st.title("K-Means")
+slider = st.slider("Iterations",1,20)
+
+X, _ = make_blobs(n_samples=1100, centers=5, n_features=2, random_state = 1)
+df = pd.DataFrame(X, columns = list("ab"))
+starting_points = np.array([[0,0],[-2,0],[-4,0],[0,2],[0,4]])
+kmeans = KMeans(n_clusters = 5, max_iter=slider, init=starting_points, n_init = 1)
+kmeans.fit(X);
+df["c"] = kmeans.predict(X)
+chart1 = alt.Chart(df).mark_circle().encode(
+    x = "a",
+    y = "b",
+    color = "c:N"
+)
+
+df_centers = pd.DataFrame(kmeans.cluster_centers_, columns = list("ab"))
+
+chart_centers = alt.Chart(df_centers).mark_point().encode(
+    x = "a",
+    y = "b",
+    color = alt.value("black"),
+    shape = alt.value("diamond"),
+)
+
+chart1 + chart_centers
+
